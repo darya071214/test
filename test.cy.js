a86801dc2e40963a441d4c859fb5c4fa028de972
darya071214 <136634933+darya071214@users.noreply.github.com>
2023-08-07 21:27:01 +0500

    Add files via upload

---
+++ test.cy.js
@@ -0,0 +1,21 @@
+import { google, search, goToLink, clickButton } from "./index.cy";
+
+describe("My first test", () => { 
+    it("Проверить ", () => { 
+        cy.visit("https://google.com"); 
+        
+        cy.get("[title='Поиск']").type("Byndyusoft").type("{enter}"); 
+        
+        cy.get("div#search a").first().click();  
+        
+        cy.origin('https://byndyusoft.com/', () => {
+            cy.get('.knowMore__container .btn').click(); 
+        })
+
+        cy.origin('https://byndyusoft.com/', () => {
+            if (cy.contains("sales@byndyusoft.com") && cy.get('[href="http://t.me/alexanderbyndyu"')){
+                alert ('Все верно')
+            } 
+        })
+    }); 
+  });
